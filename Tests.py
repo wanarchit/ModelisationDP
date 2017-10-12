2733840d01d485f01caa6e74cc5b0b16891e9b14
Delphine Rousse <delphine1237@hotmail.fr>
2017-10-12 10:16:49 +0200

    Directory Dp

--- Tests.py
+++ Tests.py
@@ -1,7 +1,7 @@
 import mdtraj as md
 
 Data_pathP = 'D:\Cours\Master 2\Modelisation Bioinformatique\p3_p4_p5_p8'
-Data_pathD = ''
+Data_pathD = 'C:\Users\Asus\Documents\Master 2 GPhy\Semestre 1\Modelisation'
 t = md.load(Data_pathP+'\md_200ns_OK.xtc', top=Data_pathP+'\start.pdb')
 print(t)
 print("tototest2")
